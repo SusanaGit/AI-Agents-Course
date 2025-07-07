29f9abd0ecdcf81db52cd966390a70dfc925d686
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2025-07-07 11:27:10 +0200

    add test.py file

--- openai_module.py
+++ openai_module.py
@@ -6,10 +6,10 @@ from openai import OpenAI
 load_dotenv()
 
 # create an instance of the OpenAI class
-openai = OpenAI(os.getenv("OPENAI_API_KEY"))
+openai_client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 def generate_text_basic(prompt: str, model = "gpt-3.5-turbo", system_prompt="You are a helpful assistant."):
-    response = openai.chat.completions.create(
+    response = openai_client.chat.completions.create(
         model = model,
         messages = [
             {"role": "system", "content": system_prompt},
---
+++ test.py
@@ -0,0 +1,8 @@
+from openai_module import generate_text_basic
+
+prompt = "generate a 5 word sentence"
+
+response = generate_text_basic(prompt)
+
+print(response)
+
